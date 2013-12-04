bc037f12ae435552922f94b3a94b8df195f2d13f
Tim Angus <tim@ngus.net>
2013-12-04 17:48:02 +0000

    Fix various issues

--- init.sql
+++ init.sql
@@ -1,6 +1,6 @@
 INSERT INTO settings (setting, value) VALUES
-    ('script-file', 'create-biolayouti-file.sh'),
+    ('script-file', '/nfs_netapp/tangus2/pipeline/create-biolayout-file.sh'),
     ('max-jobs', '8'),
     ('from-address', 'seq-graph@roslin.ed.ac.uk'),
     ('base-url', 'http://seq-graph.roslin.ed.ac.uk/'),
-    ('input-directory', './'),
+    ('input-directory', '/nfs_netapp/tangus2/pipeline/')
